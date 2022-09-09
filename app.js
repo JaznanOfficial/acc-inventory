7352e84b1801426ab14cd51f3f85cbbb1efc7de9
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-09 19:01:35 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(express.json());
+app.use(cors());
+
+// schema design
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+
+module.exports = app;
